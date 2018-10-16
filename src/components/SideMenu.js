afeffd4d9cd2499e4a948d4728d4ed2ef76e1648
hiroaki.nishi <hiroaki.nish@gu3.co.jp>
2018-10-16 18:22:58 +0900

    サイドメニューの表示を調整

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { withStyles } from '@material-ui/core/styles'
+import Divider from '@material-ui/core/Divider'
 import Drawer from '@material-ui/core/Drawer'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
 import ListItemText from '@material-ui/core/ListItemText'
 import { Link } from '@reach/router'
+
+import Alarm from '@material-ui/icons/alarm'
+import LinkIcon from '@material-ui/icons/link'
 
 const drawerWidth = 240
 
@@ -18,14 +23,22 @@ const styles = theme => ({
 })
 const SideMenu = props => {
   const { classes } = props
+
   return (
     <Drawer variant="permanent" classes={{ paper: classes.drawerPaper }}>
       <div className={classes.toolbar} />
-      <List>
+
+      <List component="nav">
         <ListItem button key="root" component={Link} to="/">
+          <ListItemIcon>
+            <Alarm />
+          </ListItemIcon>
           <ListItemText primary="Home(ルートURL)" />
         </ListItem>
         <ListItem button key="home" component={Link} to="/home">
+          <ListItemIcon>
+            <Alarm />
+          </ListItemIcon>
           <ListItemText primary="Home(違うURL)" />
         </ListItem>
         <ListItem
@@ -34,6 +47,9 @@ const SideMenu = props => {
           component={Link}
           to="/sample/switch"
         >
+          <ListItemIcon>
+            <LinkIcon />
+          </ListItemIcon>
           <ListItemText primary="Switch サンプル" />
         </ListItem>
         <ListItem
@@ -44,10 +60,20 @@ const SideMenu = props => {
         >
           <ListItemText primary="Button サンプル" />
         </ListItem>
-        <ListItem button key="link_sample" component={Link} to="/sample/link">
+        <ListItem
+          button
+          key="link_sample"
+          component={Link}
+          to="/sample/link"
+        >
           <ListItemText primary="Link サンプル" />
         </ListItem>
-        <ListItem button key="table_sample" component={Link} to="/sample/table">
+        <ListItem
+          button
+          key="table_sample"
+          component={Link}
+          to="/sample/table"
+        >
           <ListItemText primary="Table サンプル" />
         </ListItem>
         <ListItem
@@ -57,6 +83,19 @@ const SideMenu = props => {
           to="/sample/dialog"
         >
           <ListItemText primary="Dialog サンプル" />
+        </ListItem>
+      </List>
+
+      <Divider />
+
+      <List component="nav">
+        <ListItem button key="icons_list">
+          <a
+            target="_blank"
+            href="https://material.io/tools/icons/?style=baseline"
+          >
+            参考 アイコン一覧
+          </a>
         </ListItem>
       </List>
     </Drawer>
